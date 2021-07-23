3ed38129951eaed8cc0e05c7198fecca44515e5d
Iryna <iryna.shulim@gmail.com>
2021-07-23 13:07:44 -0500

    role table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER
 );
