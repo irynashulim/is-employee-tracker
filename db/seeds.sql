56d6ea2cfbfbe1cd5c574d6bb2597b89f83449ff
Iryna <iryna.shulim@gmail.com>
2021-07-29 11:50:59 -0500

    role values added to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,20 @@ VALUES
 ('IT'),
 ('Sales'),
 ('Finance'),
-('Legal');
+('Legal'),
+('Marketing');
+
+INSERT INTO role (title, salary, department_id) 
+VALUES
+('HR Manager', '75000', 1),
+('HR Admin', '50000', 1),
+('IT Manager', '100000', 2),
+('IT Admin', '65000', 2),
+('Sales Manager', '70000', 3),
+('Salesperson', '40000', 3),
+('Finance Manager', '80000', 4),
+('Accountant', '65000', 4),
+('Chief Legal Offiver', '120000', 5),
+('Lowyer', '80000', 5),
+('Marketing Manager', '80000', 6),
+('Marketing Admin', '50000', 6);
